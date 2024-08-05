16c7400aec8fca5d20eb375676a046aa5ec59ab3
eddieljs <2574350328@qq.com>
2024-08-05 15:24:05 +0800

    用户统计

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -8,6 +8,7 @@ import com.sky.result.Result;
 import com.sky.service.CategoryService;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -33,6 +34,12 @@ public class ReportController {
     @Autowired
     private ReportService reportService;
 
+    /**
+     * 营业额统计
+     * @param begin
+     * @param end
+     * @return
+     */
     @GetMapping("/turnoverStatistics")
     @ApiOperation("营业额统计")
     public Result<TurnoverReportVO> turnoverStatistics(
@@ -42,5 +49,22 @@ public class ReportController {
         TurnoverReportVO turnoverStatistics = reportService.getTurnoverStatistics(begin, end);
 
         return Result.success(turnoverStatistics);
+    }
+
+    /**
+     * 用户数据统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    @GetMapping("/userStatistics")
+    @ApiOperation("用户数据统计")
+    public Result<UserReportVO> userStatistics(
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd")LocalDate end){
+        log.info("用户数据统计：{}，{}",begin,end);
+        UserReportVO userStatistics = reportService.getUserStatistics(begin, end);
+
+        return Result.success(userStatistics);
     }
 }
--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -1,9 +1,12 @@
 package com.sky.service.impl;
 
 import com.sky.entity.Orders;
+import com.sky.entity.User;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.apache.poi.util.StringUtil;
@@ -30,6 +33,9 @@ public class ReportServiceImpl implements ReportService {
 
     @Autowired
     private OrderMapper orderMapper;
+
+    @Autowired
+    private UserMapper userMapper;
 
     /**
      * 查询营业额
@@ -71,5 +77,51 @@ public class ReportServiceImpl implements ReportService {
                         .dateList(listToString)
                         .turnoverList(StringUtils.join(turnoverList,","))
                         .build();
+    }
+
+    /**
+     * 查询用户数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    @Override
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+        //存放从begin到end的日期
+        List<LocalDate> dateList = new ArrayList<>();
+        dateList.add(begin);
+        while (!begin.equals(end)) {
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+
+        String listToString = StringUtils.join(dateList, ",");//将集合拼成字符串 用逗号相连
+        List<Integer> userNumberList = new ArrayList<>();
+        List<Integer> userNumberAllList = new ArrayList<>();
+        Integer userNumberAll = 0;
+        for (LocalDate date : dateList) {
+            //找到开始日期具体时间
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            //找到结束日期具体时间
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+
+            //查询营业额
+            Map map = new HashMap<>();
+            map.put("begin",beginTime);
+            map.put("end",endTime);
+            //找出每一天的用户数目
+            Integer userCount = userMapper.sumByMap(map);
+            userCount = userCount == null ? 0 : userCount;
+            userNumberList.add(userCount);
+            //对每天的用户数目累加
+            userNumberAll += userCount;
+            userNumberAllList.add(userNumberAll);
+        }
+
+        return UserReportVO.builder()
+                .dateList(listToString)
+                .newUserList(StringUtils.join(userNumberList,","))
+                .totalUserList(StringUtils.join(userNumberAllList,","))
+                .build();
     }
 }
